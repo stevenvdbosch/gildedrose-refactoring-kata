8c49386b3c1041bd41e1326e6e4b89eb3ff0cb7e
stevvanden <steven.vandenbosch@soprasteria.com>
2024-12-17 14:30:48 +0100

    refactor backstage passes

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -6,7 +6,7 @@ class GildedRose {
     Item[] items;
     List<String> specials = List.of("Aged Brie", "Backstage passes to a TAFKAL80ETC concert");
 
-    public static final int DEFAULT_QUALITY_DECREASE = 1;
+    public static final int DEFAULT_QUALITY_CHANGE = 1;
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -15,10 +15,10 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (items[i].name.startsWith("Conjured")) {
-                items[i].quality -= DEFAULT_QUALITY_DECREASE * 2;
+                items[i].quality -= DEFAULT_QUALITY_CHANGE * 2;
                 items[i].sellIn -= 1;
                 if (items[i].sellIn < 0) {
-                    items[i].quality -= DEFAULT_QUALITY_DECREASE * 2;
+                    items[i].quality -= DEFAULT_QUALITY_CHANGE * 2;
                 }
                 if (items[i].quality < 0) {
                     items[i].quality = 0;
@@ -26,6 +26,21 @@ class GildedRose {
             } else if (items[i].name.startsWith("Sulfuras")) {
                 //No change for Sulfuras
                 continue;
+            } else if (items[i].name.startsWith("Backstage passes")) {
+                items[i].quality += DEFAULT_QUALITY_CHANGE;
+                items[i].sellIn -= 1;
+                if (items[i].sellIn <= 10) {
+                    items[i].quality += DEFAULT_QUALITY_CHANGE;
+                }
+                if (items[i].sellIn <= 5) {
+                    items[i].quality += DEFAULT_QUALITY_CHANGE;
+                }
+                if(items[i].quality > 50) {
+                    items[i].quality = 50;
+                }
+                if(items[i].sellIn < 0) {
+                    items[i].quality = 0;
+                }
             } else {
                 if (!specials.contains(items[i].name)) {
                     changeQualityAndSellInForDefaultProducts(i);
@@ -37,10 +52,10 @@ class GildedRose {
     }
 
     private void changeQualityAndSellInForDefaultProducts(int i) {
-        items[i].quality -= DEFAULT_QUALITY_DECREASE;
+        items[i].quality -= DEFAULT_QUALITY_CHANGE;
         items[i].sellIn -= 1;
         if(items[i].sellIn < 0) {
-            items[i].quality -= DEFAULT_QUALITY_DECREASE;
+            items[i].quality -= DEFAULT_QUALITY_CHANGE;
         }
         if(items[i].quality < 0) {
             items[i].quality = 0;
@@ -51,20 +66,6 @@ class GildedRose {
         // change quality of items being aged brie or backstage passes
         if (items[i].quality < 50) {
             items[i].quality = items[i].quality + 1;
-
-            if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].sellIn < 11) {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-
-                if (items[i].sellIn < 6) {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
         }
 
         //change the sellIn value
@@ -72,20 +73,8 @@ class GildedRose {
 
         if (items[i].sellIn < 0) {
             // if sellin lower than 0 and item is not aged brie: lower quality extra
-            if (!items[i].name.equals("Aged Brie")) {
-                if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                    //quality of backstage passes when sellIn <0 is zero
-                } else {
-                    items[i].quality = 0;
-                }
-                //if sellin lower than 0 add quality extra
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
+            if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
             }
         }
     }
