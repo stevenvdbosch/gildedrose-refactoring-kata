5988f2a28c9f9a1b3943a7b6f41a85ad93ca0762
stevvanden <steven.vandenbosch@soprasteria.com>
2024-12-17 14:44:30 +0100

    refactoring: every item uses its own method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,44 +12,56 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (items[i].name.startsWith("Conjured")) {
-                items[i].quality -= DEFAULT_QUALITY_CHANGE * 2;
-                items[i].sellIn -= 1;
-                if (items[i].sellIn < 0) {
-                    items[i].quality -= DEFAULT_QUALITY_CHANGE * 2;
-                }
-                if (items[i].quality < 0) {
-                    items[i].quality = 0;
-                }
+                changeQualityAndSellInForConjuredItems(i);
             } else if (items[i].name.startsWith("Sulfuras")) {
                 //Sulfuras always stays the same
                 continue;
             } else if (items[i].name.startsWith("Backstage passes")) {
-                items[i].quality += DEFAULT_QUALITY_CHANGE;
-                items[i].sellIn -= 1;
-                if (items[i].sellIn <= 10) {
-                    items[i].quality += DEFAULT_QUALITY_CHANGE;
-                }
-                if (items[i].sellIn <= 5) {
-                    items[i].quality += DEFAULT_QUALITY_CHANGE;
-                }
-                if(items[i].quality > 50) {
-                    items[i].quality = 50;
-                }
-                if(items[i].sellIn < 0) {
-                    items[i].quality = 0;
-                }
+                changeQualityAndSellInForBackstagePasses(i);
             } else if (items[i].name.startsWith("Aged Brie")) {
-                items[i].quality += DEFAULT_QUALITY_CHANGE;
-                items[i].sellIn -= 1;
-                if(items[i].sellIn < 0) {
-                    items[i].quality += DEFAULT_QUALITY_CHANGE;
-                }
-                if(items[i].quality > 50) {
-                    items[i].quality = 50;
-                }
+                changeQualityAndSellInForAgedBrie(i);
             } else {
                 changeQualityAndSellInForDefaultProducts(i);
             }
+        }
+    }
+
+    private void changeQualityAndSellInForAgedBrie(int i) {
+        items[i].quality += DEFAULT_QUALITY_CHANGE;
+        items[i].sellIn -= 1;
+        if(items[i].sellIn < 0) {
+            items[i].quality += DEFAULT_QUALITY_CHANGE;
+        }
+        if(items[i].quality > 50) {
+            items[i].quality = 50;
+        }
+    }
+
+    private void changeQualityAndSellInForBackstagePasses(int i) {
+        items[i].quality += DEFAULT_QUALITY_CHANGE;
+        items[i].sellIn -= 1;
+        if (items[i].sellIn <= 10) {
+            items[i].quality += DEFAULT_QUALITY_CHANGE;
+        }
+        if (items[i].sellIn <= 5) {
+            items[i].quality += DEFAULT_QUALITY_CHANGE;
+        }
+        if(items[i].quality > 50) {
+            items[i].quality = 50;
+        }
+        if(items[i].sellIn < 0) {
+            items[i].quality = 0;
+        }
+    }
+
+    private void changeQualityAndSellInForConjuredItems(int i) {
+        items[i].quality -= DEFAULT_QUALITY_CHANGE * 2;
+        items[i].sellIn -= 1;
+        if (items[i].sellIn < 0) {
+            items[i].quality -= DEFAULT_QUALITY_CHANGE * 2;
+        }
+        if (items[i].quality < 0) {
+            items[i].quality = 0;
         }
     }
 
